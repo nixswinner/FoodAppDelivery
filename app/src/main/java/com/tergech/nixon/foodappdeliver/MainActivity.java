52e71b3d2abc055689bafb5eb40478793a3e34ba
nixswinner <nixswinner@gmail.com>
2017-06-21 22:56:53 +0300

    Added more fragments to handle orders and loyalty points

--- app/src/main/java/com/tergech/nixon/foodappdeliver/MainActivity.java
+++ app/src/main/java/com/tergech/nixon/foodappdeliver/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 
 
 
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
 // Replace the contents of the container with the new login_fragment
         //ft.replace(R.id.your_placeholder, new daily_progress());
         ft.replace(R.id.content, new fragment_main());
@@ -38,13 +38,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                   // mTextMessage.setText(R.string.title_home);
+                    ft.replace(R.id.content, new fragment_main());
+                    ft.commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    //mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                   // mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
